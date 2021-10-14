e7c453c9b22c2ef9923e948d32427b426c7f2e83
순진 <tkksm2@gmail.com>
2021-10-14 23:37:02 +0900

    nuxt-MovieSearchProject

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { hid: 'og:site_name', property : 'og:site_name', content : 'Nuxt Movie App'},
       { hid: 'og:title', property : 'og:title', content : 'Nuxt Movie App / Search'},
       { hid: 'og:description', property : 'og:description', content : 'The OMDb API is a RESTful web service to obtain movie information, all content and images on the site are contributed and maintained by our users.'},
-      { hid: 'og:image', property : 'og:image', content : 'https://heropy.blog/css/images/logo.png'},
+      { hid: 'og:image', property : 'og:image', content : 'images/Crayon.png'},
       { hid: 'og:url', property : 'og:url', content : process.env.CLIENT_URL}
       
     ],
