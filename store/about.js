9941eb2bfb10fddd30285df69d07741d14c84bc3
순진 <tkksm2@gmail.com>
2021-10-14 23:29:32 +0900

    nuxt-MovieSearchProject

--- store/about.js
+++ store/about.js
@@ -1,10 +1,11 @@
 export default {
   namespaced: true,
   state: () => ({
-    name: 'HEROPY',
-    email: 'thesecon@gmail.com',
-    blog: 'https://heropy.blog',
-    phone: '+82-10-1234-5678',
-    image: 'https://heropy.blog/css/images/logo.png'
+    name: 'SOONMIN',
+    email: 'tkksm@naver.com',
+    blog: 'https://blog@blog',
+    phone: '+82-10-3741-9532',
+    image: 'images/Crayon.png'
+
   })
 }
